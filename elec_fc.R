72611ae4b01a07ec8589371887d6c7fbfad8c21a
Evgeniya Shtyrkova <EugeniaSH@users.noreply.github.com>
2018-09-03 14:16:31 +0200

    added prepration code

---
+++ elec_fc.R
@@ -0,0 +1,17 @@
+library(dslabs)
+library(tidyverse)
+library(dplyr)
+
+polls <- polls_us_election_2016 %>% 
+  filter(state == "U.S." & enddate >= "2016-10-31" &
+           (grade %in% c("A+","A","A-","B+") | is.na(grade))) %>% 
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100)
+
+one_poll_per_pollster <- polls %>% group_by(pollster) %>% 
+  filter(enddate == max(enddate)) %>%
+  ungroup()
+
+results <- one_poll_per_pollster %>% 
+  summarize(avg = mean(spread), se = sd(spread)/sqrt(length(spread))) %>% 
+  mutate(start = avg - 1.96*se, end = avg + 1.96*se) 
+
