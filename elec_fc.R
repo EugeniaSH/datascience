392f9b2ff8f296af16d375bb788f4f00c02652fa
Evgeniya Shtyrkova <EugeniaSH@users.noreply.github.com>
2018-09-04 11:21:50 +0200

    Added more code

--- elec_fc.R
+++ elec_fc.R
@@ -2,6 +2,8 @@ library(dslabs)
 library(tidyverse)
 library(dplyr)
 
+## Preparatory code from the previous chapter
+
 polls <- polls_us_election_2016 %>% 
   filter(state == "U.S." & enddate >= "2016-10-31" &
            (grade %in% c("A+","A","A-","B+") | is.na(grade))) %>% 
@@ -15,3 +17,70 @@ results <- one_poll_per_pollster %>%
   summarize(avg = mean(spread), se = sd(spread)/sqrt(length(spread))) %>% 
   mutate(start = avg - 1.96*se, end = avg + 1.96*se) 
 
+# Posterior distribution
+mu <- 0
+tau <- 0.035
+sigma <- results$se
+Y <- results$avg
+B <- sigma^2 / (sigma^2 + tau^2)
+
+posterior_mean <- B*mu + (1-B)*Y
+posterior_se <- sqrt( 1/ (1/sigma^2 + 1/tau^2))
+
+#Probability statement
+posterior_mean + c(-1.96, 1.96)*posterior_se
+
+#Posterior probability
+1 - pnorm(0, posterior_mean, posterior_se) #this seems to be a little bit overconfident
+
+# Simulating data points
+set.seed(3)
+J<- 6
+N <- 2000
+d <- .021
+p <- (d + 1)/2
+X <- d + rnorm(J,0,2*sqrt(p*(1-p)/N))
+
+# i pollsters and j polls from pollsters
+I <- 5
+J <- 6
+N <- 2000
+X <- sapply(1:I, function(i){
+  d + rnorm(J,0,2*sqrt(p*(1-p)/N))
+})
+
+# pollster effect model
+I <- 5
+J <- 6
+N <- 2000
+d <- .021
+p <- (d + 1)/2
+h <- rnorm(I, 0, 0.025)
+X <- sapply(1:I, function(i){
+  d + h[i] + rnorm(J,0,2*sqrt(p*(1-p)/N))
+})
+
+# general bias variability
+mu <- 0
+tau <- 0.035
+sigma <- sqrt(results$se^2 + .025^2)
+Y <- results$avg
+B <- sigma^2 / (sigma^2 + tau^2)
+
+posterior_mean <- B*mu + (1-B)*Y
+posterior_se <- sqrt( 1/ (1/sigma^2 + 1/tau^2))
+
+1 - pnorm(0, posterior_mean, posterior_se)
+
+## Electoral college ##
+
+# aggregating results from a poll taken during the last week before the election
+results <- polls_us_election_2016 %>%
+  filter(state!="U.S." & 
+           !grepl("CD", state) & 
+           enddate >="2016-10-31" & 
+           (grade %in% c("A+","A","A-","B+") | is.na(grade))) %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100) %>%
+  group_by(state) %>%
+  summarize(avg = mean(spread), sd = sd(spread), n = n()) %>%
+  mutate(state = as.character(state))
