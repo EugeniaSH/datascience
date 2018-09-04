908779431ac8e47c1558c62a0f0b0c51e0fe56b1
Evgeniya Shtyrkova <EugeniaSH@users.noreply.github.com>
2018-09-04 11:28:56 +0200

    Commit

--- elec_fc.R
+++ elec_fc.R
@@ -84,3 +84,59 @@ results <- polls_us_election_2016 %>%
   group_by(state) %>%
   summarize(avg = mean(spread), sd = sd(spread), n = n()) %>%
   mutate(state = as.character(state))
+
+results %>% arrange(abs(avg))
+
+results <- left_join(results, results_us_election_2016, by = "state")
+results_us_election_2016 %>% filter(!state %in% results$state)
+
+# assigns a standard deviation, the median of the rest, to states with just one poll
+results <- results %>%
+  mutate(sd = ifelse(is.na(sd), median(results$sd, na.rm=TRUE), sd))
+
+# posterior mean, posterior st. error
+mu <- 0
+tau <- 0.02
+results %>% mutate(sigma = sd/sqrt(n), 
+                   B = sigma^2 / (sd^2 + tau^2),
+                   posterior_mean = B*mu + (1-B)*avg,
+                   posterior_se = sqrt( 1/ (1/sigma^2 + 1/tau^2))) %>%
+  arrange(abs(posterior_mean))
+
+results %>% mutate(sigma = sd/sqrt(n), 
+                   B = sigma^2 / (sigma^2 + tau^2),
+                   posterior_mean = B*mu + (1-B)*avg,
+                   posterior_se = sqrt( 1/ (1/sigma^2 + 1/tau^2))) %>%
+  ggplot(aes(avg, posterior_mean, size = n)) + geom_point() + 
+  geom_abline(slope = 1, intercept = 0)
+
+# we keep the total number of electoral votes for Clinton
+
+mu <- 0
+tau <- 0.02
+clinton_EV <- replicate(1000, {
+  results %>% mutate(sigma = sd/sqrt(n), 
+                     B = sigma^2 / (sigma^2 + tau^2),
+                     posterior_mean = B*mu + (1-B)*avg,
+                     posterior_se = sqrt( 1/ (1/sigma^2 + 1/tau^2)),
+                     simulated_result = rnorm(length(posterior_mean), posterior_mean, posterior_se),
+                     clinton = ifelse(simulated_result>0, electoral_votes, 0)) %>% 
+    summarize(clinton = sum(clinton)) %>% 
+    .$clinton + 7## 7 for Rhode Island and D.C.
+})
+mean(clinton_EV>269)
+
+# model with general bias
+
+tau <- 0.02
+bias_sd <- 0.03
+clinton_EV_2 <- replicate(1000, {
+  results %>% mutate(sigma = sqrt(sd^2/n  + bias_sd^2),  
+                     B = sigma^2 / (sigma^2 + tau^2),
+                     posterior_mean = B*mu + (1-B)*avg,
+                     posterior_se = sqrt( 1/ (1/sigma^2 + 1/tau^2)),
+                     simulated_result = rnorm(length(posterior_mean), posterior_mean, posterior_se),
+                     clinton = ifelse(simulated_result>0, electoral_votes, 0)) %>% 
+    summarize(clinton = sum(clinton) + 7) %>% .$clinton ## 7 for Rhode Island and D.C.
+})
+mean(clinton_EV_2>269)
